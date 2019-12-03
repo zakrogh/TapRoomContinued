c16e90c690a7ca78b568800f3bd64bf05add91d8
Student <example@example.com>
2019-12-03 10:15:04 -0800

    initialize project with template/boilerplate files

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={todo:HOMEPAGE} />
+        <Route path='/taplist' component={todo:TAPLIST} />
+        <Route component={Error404} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header(){
+  return (
+    <div>
+      <h1>Tap Room</h1>
+      <Link to="/">Home</Link> | <Link to="/taplist">Tap List</Link>
+    </div>
+  );
+}
+
+export default Header;
