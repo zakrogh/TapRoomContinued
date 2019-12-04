c828810cfcefde57439ed0506c616debc03ecd42
Student <example@example.com>
2019-12-04 09:53:14 -0800

    refactor App.jsx to handle dynamic adding of beers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,44 @@ import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 import TapList from './TapList';
 import Welcome from './Welcome';
+/*eslint-disable*/
+import Moment from 'moment';
+import { v4 } from 'uuid';
+/*eslint-enable*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterBeerList: {},
+      selectedBeer: null
+    };
+    this.handleAddingNewBeerToList = this.handleAddingNewBeerToList.bind(this);
+    this.handleChangingSelectedBeer = this.handleChangingSelectedBeer.bind(this);
+  }
 
-function App(){
-  return (
-    <div>
-      <Header/>
-      <div className='container'>
-        <Switch>
-          <Route exact path='/' component={Welcome} />
-          <Route path='/taplist' component={TapList} />
-          <Route component={Error404} />
-        </Switch>
+  handleChangingSelectedBeer(beerId){
+    this.setState({selectedBeer: beerId});
+  }
+
+  componentDidMount() {
+  }
+  componentWillUnmount(){
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Welcome} />
+            <Route path='/taplist' component={TapList} />
+            <Route component={Error404} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
