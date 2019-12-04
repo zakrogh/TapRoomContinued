d4e83314de30a047a1b72dd220df4da5c28325fc
Student <example@example.com>
2019-12-04 10:26:26 -0800

    add control file for adding beers

---
+++ src/components/NewBeerControl.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NewBeerForm from './NewBeerForm';
+import PropTypes from 'prop-types';
+
+class NewBeerControl extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <NewBeerForm onNewBeerCreation={this.props.onNewBeerCreation}/>;
+      </div>
+    );
+  }
+}
+NewBeerControl.propTypes = {
+  onNewBeerCreation: PropTypes.func
+};
+
+export default NewBeerControl;
